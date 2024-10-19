2a600a8be4e4c06e11873203f47d7827d0c10910
the real Slyce @ pc <91631241+SlyceDF@users.noreply.github.com>
2024-10-19 22:43:52 +0200

    v1.14.6
    - patch jei itemhiding

--- curse/overrides/kubejs/client_scripts/jei.js
+++ curse/overrides/kubejs/client_scripts/jei.js
@@ -1,76 +1,14 @@
+/* let removed;
+
+onEvent('item.tags', event => {
+  removed = event.get('kubejs:removed').getObjectIds()
+}) */
+
 onEvent('jei.hide.items', event => {
-	event.hide('farmersdelight:wheat_dough')
-	event.hide('kubejs:incomplete_kinetic_mechanism')
-	event.hide('kubejs:incomplete_kinetic_mechanism_x4')
-                event.hide('kubejs:incomplete_hydraulic_mechanism')
-                event.hide('kubejs:incomplete_sturdy_mechanism')
-                event.hide('kubejs:incomplete_inductive_mechanism')
-                event.hide('kubejs:incomplete_universal_mechanism')
-                event.hide('kubejs:incomplete_abstruse_mechanism')
-                event.hide('kubejs:incomplete_pie')
-                event.hide('kubejs:incomplete_cake')
-                event.hide('kubejs:incomplete_magmatic_mechanism') 
-                event.hide('kubejs:incomplete_redstone_circuit_plate') 
-                event.hide('kubejs:incomplete_infernal_mechanism') 
-                event.hide('kubejs:booting_assembly') 
-            
-event.hide('kubejs:incomplete_lapis_alloy')
-event.hide('kubejs:incomplete_amethyst_alloy')
-event.hide('kubejs:growing_amethyst_seed')
-event.hide('kubejs:growing_mythril_seed')
-event.hide('kubejs:growing_quartz_cluster')
-event.hide('ae2:silicon')
-
-event.hide('kubejs:incomplete_generating_coil')
-event.hide('kubejs:incomplete_transmitting_coil')
-event.hide('kubejs:incomplete_absorbing_coil')
-event.hide('kubejs:incomplete_inductive_coil')
-    
-event.hide('chemlib:lead_metal_block') 
-event.hide('chemlib:lead_plate') 
-event.hide('chemlib:lead_dust') 
-event.hide('chemlib:lead_ingot') 
-
-event.hide('chemlib:nickel_metal_block') 
-event.hide('chemlib:nickel_plate') 
-event.hide('chemlib:nickel_dust') 
-event.hide('chemlib:nickel_ingot') 
-
-event.hide('chemlib:silver_metal_block') 
-event.hide('chemlib:silver_plate') 
-event.hide('chemlib:silver_dust') 
-event.hide('chemlib:silver_ingot') 
-
-event.hide('chemlib:tin_metal_block') 
-event.hide('chemlib:tin_plate') 
-event.hide('chemlib:tin_dust') 
-event.hide('chemlib:tin_ingot') 
-
-  event.hide('tconstruct:copper_nugget')
-  event.hide('alloyed:steel_nugget')
-  event.hide('overchisel:copper_plate')
-  event.hide('overchisel:copper_rod')
-  event.hide('chemlib:copper_plate')
-  event.hide('chemlib:gold_plate')
-  event.hide('chemlib:iron_plate')
-
-  event.hide('chemlib:zinc_ingot')
-  event.hide('chemlib:zinc_nugget')
-  event.hide('chemlib:zinc_metal_block')
-  event.hide('chemlib:zinc_plate')
-
-  event.hide('chemlib:cobalt_ingot')
-  event.hide('chemlib:cobalt_nugget')
-  event.hide('chemlib:cobalt_metal_block')
-event.hide('tconstruct:honey_bucket')
-event.hide('createsifter:custom_mesh')
-event.hide('pneumaticcraft:vegetable_oil_bucket')
-
-event.hide('quark:deepslate_furnace')
-event.hide('quark:blackstone_furnace')
-event.hide('#c:workbenches')
-event.hide('#kubejs:removed')
+  // removed.forEach(ritem => event.hide(ritem))
+  event.hide('#kubejs:removed')
 })
+
 onEvent('jei.add.items', event => {
 event.add('minecraft:sculk_sensor')
 event.add('minecraft:light')
